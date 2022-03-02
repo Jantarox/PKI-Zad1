4aafe76164817081d535af3cf67179533a83768c
Jantarox <228807@edu.p.lodz.pl>
2022-03-02 16:00:28 +0100

    zmiany przez SourceTree

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     static Integer policz(Integer a, Integer b){
-        return a*b;
+        return (a+b)*(a-b);
     }
 
     public static void main(String[] args) {
         System.out.println("W pierwszym branchu: 2+3 = ".concat(Main.policz(2, 3).toString()));
-    }
-
     }
 }
