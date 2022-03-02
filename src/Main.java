cbfaa6fc8b4071f147d96f8d41dda340118bbff7
Jantarox <228807@edu.p.lodz.pl>
2022-03-02 15:34:36 +0100

    Nowa metoda policz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-
+    static Integer policz(Integer a, Integer b){
+        return a+b;
     }
+
+    public static void main(String[] args) {
+        System.out.println("W pierwszym branchu: 2+3 = ".concat(Main.policz(2, 3).toString()));
+    }
+
+
 }
